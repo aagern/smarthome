4cd0751e8b1f54038ce71940aff0e48af0879508
Alex Rybalko <aagern@gmail.com>
2026-03-25 03:15:18 +0300

    Room struct and methods added

--- home/src/lib.rs
+++ home/src/lib.rs
@@ -2,6 +2,7 @@
 use anyhow::{Context, Result};
 use tracing::{debug, error, info, warn};
 
+// Реальные показания с датчиков
 #[cfg(not(feature = "mock"))]
 mod real;
 #[cfg(not(feature = "mock"))]
@@ -9,6 +10,7 @@ use real::SmartSocket;
 #[cfg(not(feature = "mock"))]
 pub use real::SmartThermometer;
 
+// Случайные числа в качестве показаний
 #[cfg(feature = "mock")]
 mod mock;
 #[cfg(feature = "mock")]
@@ -16,9 +18,30 @@ use mock::SmartSocket;
 #[cfg(feature = "mock")]
 pub use mock::SmartThermometer;
 
-enum SmartDevice {
+pub enum SmartDevice {
     SmartThermometer(SmartThermometer),
     SmartSocket(SmartSocket),
+}
+
+pub struct Room {
+    devices: Vec<SmartDevice>,
+}
+
+impl Room {
+    /// Конструктор, принимающий массив устройств
+    pub fn new(devices: Vec<SmartDevice>) -> Self {
+        Room { devices }
+    }
+
+    /// Получить ссылку на устройство по индексу
+    pub fn get_device(&self, index: usize) -> Option<&SmartDevice> {
+        self.devices.get(index)
+    }
+
+    /// Получить изменяемую ссылку на устройство по индексу
+    pub fn get_device_mut(&mut self, index: usize) -> Option<&mut SmartDevice> {
+        self.devices.get_mut(index)
+    }
 }
 
 #[cfg(test)]
